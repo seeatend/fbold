2dff994362d8da6ce975a4e514e218f13438854f
Thomas Kane <thomas@fluxbucket.com>
2018-02-19 15:00:26 -0600

    versioning vendor directory

---
+++ vendor/andreyco/instagram/example/index.php
@@ -0,0 +1,56 @@
+<?php
+
+// ini_set('display_errors', 'off');
+
+require '../vendor/autoload.php';
+
+$instagram = new Andreyco\Instagram\Client(array(
+  'apiKey'      => 'YOUR_APP_KEY',
+  'apiSecret'   => 'YOUR_APP_SECRET',
+  'apiCallback' => 'YOUR_APP_CALLBACK',
+  'scope'      => array('basic', 'comments', 'relationships', 'likes'),
+));
+
+// create login URL
+$state = md5(time());
+$loginUrl = $instagram->getLoginUrl(array('relationships'), $state);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram - OAuth Login</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <style>
+      .login {
+        display: block;
+        font-size: 20px;
+        font-weight: bold;
+        margin-top: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <header class="clearfix">
+        <h1>Instagram <span>display your photo stream</span></h1>
+      </header>
+      <div class="main">
+        <ul class="grid">
+          <li><img src="assets/instagram-big.png" alt="Instagram logo"></li>
+          <li>
+            <a class="login" href="<? echo $loginUrl ?>">» Login with Instagram</a>
+            <h>Use your Instagram account to login.</h4>
+          </li>
+        </ul>
+        <!-- GitHub project -->
+        <footer>
+          <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>, available on GitHub</p>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
