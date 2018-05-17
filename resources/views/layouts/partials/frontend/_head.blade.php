139a3aa736ef7be810ed6fbe4191e789e53f3c23
Thomas Kane <thomasjohnkane@gmail.com>
2018-05-17 16:11:00 -0500

    updates to remove user scalable

--- resources/views/layouts/partials/frontend/_head.blade.php
+++ resources/views/layouts/partials/frontend/_head.blade.php
@@ -1,8 +1,8 @@
 <head>
     <meta content="text/html; charset=UTF-8" http-equiv="content-type"> 
     <title>@yield('pageTitle', 'FollowBack')</title>
-    {{-- <meta name="viewport" content="width=device-width, initial-scale=.5, maximum-scale=.5" /> --}}
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=.5, maximum-scale=.5" />
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"> --}}
     {{asset_stylesheet('bootstrap/bootstrap.min.css')}}
     {{asset_stylesheet('application.min.css')}}
     {{asset_stylesheet('plugins/toastr/toastr.min.css')}}
